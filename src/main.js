cd59f8aade5d0b2f19ffcbef062028c9a45927bd
zewei <543347844@qq.com>
2018-07-23 09:57:56 +0800

    404页面

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './vuex';
 import App from './App';
 
 import "@src/assets/icons/style.css";
-import "@src/assets/scss-pc/reset.scss";
+import "@src/assets/scss/reset.scss";
 
 import 'element-ui/lib/theme-chalk/index.css' // 默认的皮肤
 // import './assets/scss-pc/theme-change.scss' // 更改皮肤
--- src/views/error/error.vue
+++ src/views/error/error.vue
@@ -1,10 +1,65 @@
 <template>
-  <div>错误页面</div>
+  <div class="error-404">
+    <div class="box">
+      <h3>404</h3>
+      <p>该页面不存在⊙﹏⊙‖∣°</p>
+      <button @click="$router.back()">返回上一页</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.error-404 {
+  display: flex;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  align-items: center;
+  text-align: center;
+
+  .box {
+    margin: 0 auto;
+  }
+
+  h3 {
+    font-size: 120px;
+    color: #cfcfcf;
+  }
+
+  p {
+    color: #999;
+    margin-bottom: 30px;
+  }
+
+  button {
+    display: inline-block;
+    line-height: 1;
+    white-space: nowrap;
+    cursor: pointer;
+    background: #42b983;
+    border: 1px solid #42b983;
+    color: #606266;
+    -webkit-appearance: none;
+    text-align: center;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    outline: 0;
+    margin: 0;
+    -webkit-transition: 0.1s;
+    transition: 0.1s;
+    padding: 12px 20px;
+    font-size: 14px;
+    color: #fff;
+  }
+
+  button:active {
+    opacity: 0.8;
+  }
+}
 </style>
